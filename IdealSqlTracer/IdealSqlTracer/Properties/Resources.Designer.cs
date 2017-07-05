2cc4fa9228ec0ebb4d4db1268219d08448683901
harvey007y <harvey007y@gmail.com>
2017-07-05 15:08:00 -0500

    Updated Summary and fixed filters

--- IdealSqlTracer/IdealSqlTracer/Properties/Resources.Designer.cs
+++ IdealSqlTracer/IdealSqlTracer/Properties/Resources.Designer.cs
@@ -63,16 +63,6 @@ namespace IdealSqlTracer.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap GreenBox {
-            get {
-                object obj = ResourceManager.GetObject("GreenBox", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap RedArrowDown {
             get {
                 object obj = ResourceManager.GetObject("RedArrowDown", resourceCulture);
